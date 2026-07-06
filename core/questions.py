50ed4c26112df7a89f04806bcbca10e230b46cd9
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-06 15:20:59 +0300

    Update questions.py

--- core/questions.py
+++ core/questions.py
@@ -1,1 +1,12 @@
-
+questions = [
+    {
+        "q": "What is 2 + 2?",
+        "options": ["1", "2", "4", "5"],
+        "answer": "4"
+    },
+    {
+        "q": "Capital of Ethiopia?",
+        "options": ["Addis Ababa", "Nairobi", "Cairo", "London"],
+        "answer": "Addis Ababa"
+    }
+]
