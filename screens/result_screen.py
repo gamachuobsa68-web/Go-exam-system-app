a9fe04b0aa6795ddf70b8812a22db435ff3870b3
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-06 15:27:01 +0300

    Update result_screen.py

--- screens/result_screen.py
+++ screens/result_screen.py
@@ -1,1 +1,10 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from core.score import score
 
+class ResultScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+
+        self.add_widget(Label(text=f"Your Score: {score}"))
