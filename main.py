ec310566b78d9e0a2662b9e67f2b9c2e0d5cce3c
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-06 15:27:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from screens.home import HomeScreen
+from screens.exam import ExamScreen
+from screens.result import ResultScreen
+
+class Root(BoxLayout):
+    pass
 
 class OroExam(App):
     def build(self):
-        return Label(text="Oro Exam System Started")
+        self.root = BoxLayout()
+
+        self.home()
+
+        return self.root
+
+    def home(self):
+        self.root.clear_widgets()
+        self.root.add_widget(HomeScreen(self.start_exam))
+
+    def start_exam(self, instance):
+        self.root.clear_widgets()
+        self.root.add_widget(ExamScreen(self.show_result))
+
+    def show_result(self):
+        self.root.clear_widgets()
+        self.root.add_widget(ResultScreen())
 
 OroExam().run()
